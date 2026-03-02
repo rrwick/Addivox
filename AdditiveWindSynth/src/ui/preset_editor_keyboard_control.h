131d99f2b98c8e4b02f7589f0a063886891c2be5
Ryan Wick <rrwick@gmail.com>
2026-03-03 06:03:56 +1100

    Use dots to show key notes

--- AdditiveWindSynth/src/ui/preset_editor_keyboard_control.h
+++ AdditiveWindSynth/src/ui/preset_editor_keyboard_control.h
@@ -56,16 +56,12 @@ public:
   template <size_t N>
   void SetHighlightedMidiNotes(const std::array<int, N>& midiNotes)
   {
-    ClearHighlightedMidiNotes();
-    for(const int midiNote : midiNotes)
-      SetHighlightedMidiNote(midiNote, true);
+    ReplaceHighlightedMidiNotes(midiNotes);
   }
 
   void SetHighlightedMidiNotes(std::initializer_list<int> midiNotes)
   {
-    ClearHighlightedMidiNotes();
-    for(const int midiNote : midiNotes)
-      SetHighlightedMidiNote(midiNote, true);
+    ReplaceHighlightedMidiNotes(midiNotes);
   }
 
   void SetSelectedMidiNote(int midiNote)
@@ -92,9 +88,7 @@ public:
   {
     if(mEditMode)
     {
-      const int keyIndex = GetKeyAtPointForEditMode(x, y);
-      if(keyIndex >= 0)
-        SetSelectedMidiNote(GetMidiNoteNumberForKey(keyIndex));
+      SelectNoteAtPoint(x, y);
       return;
     }
 
@@ -113,9 +107,7 @@ public:
   {
     if(mEditMode)
     {
-      const int keyIndex = GetKeyAtPointForEditMode(x, y);
-      if(keyIndex >= 0)
-        SetSelectedMidiNote(GetMidiNoteNumberForKey(keyIndex));
+      SelectNoteAtPoint(x, y);
       return;
     }
 
@@ -141,9 +133,56 @@ public:
   }
 
 private:
+  static constexpr int kMaxUpperOcclusions = 4;
+
+  struct KeyGeometry
+  {
+    int numKeys = 0;
+    float blackKeyBottom = 0.f;
+    float blackKeyWidth = 0.f;
+  };
+
+  struct KeyNoteDotGeometry
+  {
+    float whiteRegionTop = 0.f;
+    float radius = 0.f;
+    float bottomOffset = 0.f;
+  };
+
+  struct Interval
+  {
+    float l = 0.f;
+    float r = 0.f;
+  };
+
+  template <typename TMidiNotes>
+  void ReplaceHighlightedMidiNotes(const TMidiNotes& midiNotes)
+  {
+    ClearHighlightedMidiNotes();
+    for(const int midiNote : midiNotes)
+      SetHighlightedMidiNote(midiNote, true);
+  }
+
   static bool IsValidMidiNote(int midiNote)
   {
     return midiNote >= 0 && midiNote <= 127;
+  }
+
+  KeyGeometry GetKeyGeometry() const
+  {
+    const int numKeys = mMaxNote - mMinNote + 1;
+    if(numKeys <= 0)
+      return {};
+
+    return {numKeys, mRECT.T + mRECT.H() * mBKHeightRatio, (numKeys > 1) ? (mWKWidth * mBKWidthRatio) : mWKWidth};
+  }
+
+  KeyNoteDotGeometry GetKeyNoteDotGeometry(const KeyGeometry& geometry) const
+  {
+    const float whiteRegionTop = std::clamp(geometry.blackKeyBottom, mRECT.T, mRECT.B);
+    const float whiteRegionHeight = std::max(1.f, mRECT.B - whiteRegionTop);
+    const float radius = std::min(mWKWidth, whiteRegionHeight) * mWhiteKeyDotRadiusFactor;
+    return {whiteRegionTop, radius, whiteRegionHeight * 0.5f};
   }
 
   bool IsMidiNoteInRange(int midiNote) const
@@ -164,30 +203,34 @@ private:
     return midiNote == mSelectedMidiNote;
   }
 
+  void SelectNoteAtPoint(float x, float y)
+  {
+    const int keyIndex = GetKeyAtPointForEditMode(x, y);
+    if(keyIndex >= 0)
+      SetSelectedMidiNote(GetMidiNoteNumberForKey(keyIndex));
+  }
+
   int GetKeyAtPointForEditMode(float x, float y) const
   {
     IRECT clipRect = mRECT.GetPadded(-2.f);
     clipRect.Constrain(x, y);
 
-    const int numKeys = mMaxNote - mMinNote + 1;
-    if(numKeys <= 0)
+    const KeyGeometry geometry = GetKeyGeometry();
+    if(geometry.numKeys <= 0)
       return -1;
 
-    const float blackKeyBottom = mRECT.T + mRECT.H() * mBKHeightRatio;
-    const float blackKeyWidth = (numKeys > 1) ? (mWKWidth * mBKWidthRatio) : mWKWidth;
-
-    for(int key = 0; key < numKeys; ++key)
+    for(int key = 0; key < geometry.numKeys; ++key)
     {
       if(!mIsBlackKeyList.Get()[key])
         continue;
 
       const float keyLeft = mKeyXPos.Get()[key];
-      const IRECT keyBounds{keyLeft, mRECT.T, keyLeft + blackKeyWidth, blackKeyBottom};
+      const IRECT keyBounds{keyLeft, mRECT.T, keyLeft + geometry.blackKeyWidth, geometry.blackKeyBottom};
       if(keyBounds.Contains(x, y))
         return key;
     }
 
-    for(int key = 0; key < numKeys; ++key)
+    for(int key = 0; key < geometry.numKeys; ++key)
     {
       if(mIsBlackKeyList.Get()[key])
         continue;
@@ -201,20 +244,83 @@ private:
     return -1;
   }
 
-  const IColor& GetWhiteOverlayColor(bool keyNote, bool selected) const
+  int CollectUpperOcclusions(float keyLeft,
+                             float keyRight,
+                             const KeyGeometry& geometry,
+                             std::array<Interval, kMaxUpperOcclusions>& occlusions) const
   {
-    if(selected)
-      return keyNote ? mSelectedKeyNoteWhiteColor : mSelectedNonKeyNoteWhiteColor;
+    int numOcclusions = 0;
+    for(int key = 0; key < geometry.numKeys; ++key)
+    {
+      if(!mIsBlackKeyList.Get()[key])
+        continue;
 
-    return keyNote ? mUnselectedKeyNoteWhiteColor : mUnselectedNonKeyNoteWhiteColor;
+      const float blackLeft = mKeyXPos.Get()[key];
+      const float blackRight = blackLeft + geometry.blackKeyWidth;
+      const float overlapLeft = std::max(keyLeft, blackLeft);
+      const float overlapRight = std::min(keyRight, blackRight);
+      if(overlapRight <= overlapLeft)
+        continue;
+
+      if(numOcclusions < static_cast<int>(occlusions.size()))
+        occlusions[static_cast<size_t>(numOcclusions++)] = {overlapLeft, overlapRight};
+    }
+
+    return numOcclusions;
   }
 
-  const IColor& GetBlackOverlayColor(bool keyNote, bool selected) const
+  void DrawSelectedWhiteKeyFill(IGraphics& g, int key, const KeyGeometry& geometry, const IColor& fillColor)
   {
-    if(selected)
-      return keyNote ? mSelectedKeyNoteBlackColor : mSelectedNonKeyNoteBlackColor;
+    const float keyLeft = mKeyXPos.Get()[key];
+    const float keyRight = keyLeft + mWKWidth;
 
-    return keyNote ? mUnselectedKeyNoteBlackColor : mUnselectedNonKeyNoteBlackColor;
+    if(geometry.blackKeyBottom < mRECT.B)
+      g.FillRect(fillColor, {keyLeft, geometry.blackKeyBottom, keyRight, mRECT.B}, &mBlend);
+
+    if(geometry.blackKeyBottom <= mRECT.T)
+      return;
+
+    std::array<Interval, kMaxUpperOcclusions> occlusions{};
+    const int numOcclusions = CollectUpperOcclusions(keyLeft, keyRight, geometry, occlusions);
+    if(numOcclusions == 0)
+    {
+      g.FillRect(fillColor, {keyLeft, mRECT.T, keyRight, geometry.blackKeyBottom}, &mBlend);
+      return;
+    }
+
+    std::sort(occlusions.begin(),
+              occlusions.begin() + numOcclusions,
+              [](const Interval& a, const Interval& b) { return a.l < b.l; });
+
+    float visibleStart = keyLeft;
+    for(int i = 0; i < numOcclusions; ++i)
+    {
+      const Interval& occlusion = occlusions[static_cast<size_t>(i)];
+      if(occlusion.l > visibleStart)
+        g.FillRect(fillColor, {visibleStart, mRECT.T, occlusion.l, geometry.blackKeyBottom}, &mBlend);
+
+      visibleStart = std::max(visibleStart, occlusion.r);
+    }
+
+    if(visibleStart < keyRight)
+      g.FillRect(fillColor, {visibleStart, mRECT.T, keyRight, geometry.blackKeyBottom}, &mBlend);
+  }
+
+  void DrawWhiteKeyNoteDot(IGraphics& g, int key, const KeyNoteDotGeometry& dotGeometry)
+  {
+    const float keyMidX = mKeyXPos.Get()[key] + (mWKWidth * 0.5f);
+    const float dotCenterY = std::clamp(mRECT.B - dotGeometry.bottomOffset,
+                                        dotGeometry.whiteRegionTop + dotGeometry.radius,
+                                        mRECT.B - dotGeometry.radius);
+    g.FillCircle(mKeyNoteDotColor, keyMidX, dotCenterY, dotGeometry.radius, &mBlend);
+  }
+
+  void DrawBlackKeyNoteDot(IGraphics& g, const IRECT& keyBounds, const KeyNoteDotGeometry& dotGeometry)
+  {
+    const float dotCenterY = std::clamp(keyBounds.B - dotGeometry.bottomOffset,
+                                        keyBounds.T + dotGeometry.radius,
+                                        keyBounds.B - dotGeometry.radius);
+    g.FillCircle(mKeyNoteDotColor, keyBounds.MW(), dotCenterY, dotGeometry.radius, &mBlend);
   }
 
   void EndMouseNoteIfNeeded()
@@ -238,127 +344,58 @@ private:
 
   void DrawEditModeHighlights(IGraphics& g)
   {
-    const int numKeys = mMaxNote - mMinNote + 1;
-    if(numKeys <= 0)
+    const KeyGeometry geometry = GetKeyGeometry();
+    if(geometry.numKeys <= 0)
       return;
 
-    const float blackKeyBottom = mRECT.T + mRECT.H() * mBKHeightRatio;
-    const float blackKeyWidth = (numKeys > 1) ? (mWKWidth * mBKWidthRatio) : mWKWidth;
+    const IColor selectedWhiteFillColor = mPK_COLOR;
+    IColor selectedBlackFillColor = mPK_COLOR;
+    selectedBlackFillColor.A = static_cast<int>(mBKAlpha);
+    const KeyNoteDotGeometry dotGeometry = GetKeyNoteDotGeometry(geometry);
 
-    for(int key = 0; key < numKeys; ++key)
+    for(int key = 0; key < geometry.numKeys; ++key)
     {
-      const bool isBlack = mIsBlackKeyList.Get()[key];
-      if(isBlack)
+      if(mIsBlackKeyList.Get()[key])
         continue;
 
       const int midiNote = mMinNote + key;
-      const bool isKeyNote = IsMidiNoteHighlighted(midiNote);
       const bool isSelected = IsMidiNoteSelected(midiNote);
-      const IColor& whiteOverlayColor = GetWhiteOverlayColor(isKeyNote, isSelected);
-      if(whiteOverlayColor.A == 0)
+      const bool isKeyNote = IsMidiNoteHighlighted(midiNote);
+      if(!isSelected && !isKeyNote)
         continue;
 
-      const float keyLeft = mKeyXPos.Get()[key];
-      const float keyRight = keyLeft + mWKWidth;
+      if(isSelected)
+        DrawSelectedWhiteKeyFill(g, key, geometry, selectedWhiteFillColor);
 
-      // Bottom portion of white keys is always visible.
-      if(blackKeyBottom < mRECT.B)
-      {
-        const IRECT lowerWhiteBounds{keyLeft, blackKeyBottom, keyRight, mRECT.B};
-        g.FillRect(whiteOverlayColor, lowerWhiteBounds, &mBlend);
-      }
-
-      // Top portion avoids the horizontal spans covered by overlapping black keys.
-      if(blackKeyBottom > mRECT.T)
-      {
-        struct Interval
-        {
-          float l;
-          float r;
-        };
-
-        std::array<Interval, 4> occludedIntervals{};
-        int numOccludedIntervals = 0;
-
-        for(int blackKey = 0; blackKey < numKeys; ++blackKey)
-        {
-          if(!mIsBlackKeyList.Get()[blackKey])
-            continue;
-
-          const float blackLeft = mKeyXPos.Get()[blackKey];
-          const float blackRight = blackLeft + blackKeyWidth;
-          const float overlapLeft = std::max(keyLeft, blackLeft);
-          const float overlapRight = std::min(keyRight, blackRight);
-          if(overlapRight <= overlapLeft)
-            continue;
-
-          if(numOccludedIntervals < static_cast<int>(occludedIntervals.size()))
-            occludedIntervals[static_cast<size_t>(numOccludedIntervals++)] = {overlapLeft, overlapRight};
-        }
-
-        if(numOccludedIntervals == 0)
-        {
-          const IRECT upperWhiteBounds{keyLeft, mRECT.T, keyRight, blackKeyBottom};
-          g.FillRect(whiteOverlayColor, upperWhiteBounds, &mBlend);
-        }
-        else
-        {
-          std::sort(occludedIntervals.begin(),
-                    occludedIntervals.begin() + numOccludedIntervals,
-                    [](const Interval& a, const Interval& b) { return a.l < b.l; });
-
-          float visibleStart = keyLeft;
-          for(int intervalIndex = 0; intervalIndex < numOccludedIntervals; ++intervalIndex)
-          {
-            const Interval& occluded = occludedIntervals[static_cast<size_t>(intervalIndex)];
-
-            if(occluded.l > visibleStart)
-            {
-              const IRECT visibleUpperWhiteBounds{visibleStart, mRECT.T, occluded.l, blackKeyBottom};
-              g.FillRect(whiteOverlayColor, visibleUpperWhiteBounds, &mBlend);
-            }
-
-            visibleStart = std::max(visibleStart, occluded.r);
-          }
-
-          if(visibleStart < keyRight)
-          {
-            const IRECT trailingUpperWhiteBounds{visibleStart, mRECT.T, keyRight, blackKeyBottom};
-            g.FillRect(whiteOverlayColor, trailingUpperWhiteBounds, &mBlend);
-          }
-        }
-      }
+      if(isKeyNote)
+        DrawWhiteKeyNoteDot(g, key, dotGeometry);
     }
 
-    for(int key = 0; key < numKeys; ++key)
+    for(int key = 0; key < geometry.numKeys; ++key)
     {
-      const bool isBlack = mIsBlackKeyList.Get()[key];
-      if(!isBlack)
+      if(!mIsBlackKeyList.Get()[key])
         continue;
 
       const int midiNote = mMinNote + key;
-      const bool isKeyNote = IsMidiNoteHighlighted(midiNote);
       const bool isSelected = IsMidiNoteSelected(midiNote);
-      const IColor& blackOverlayColor = GetBlackOverlayColor(isKeyNote, isSelected);
-      if(blackOverlayColor.A == 0)
+      const bool isKeyNote = IsMidiNoteHighlighted(midiNote);
+      if(!isSelected && !isKeyNote)
         continue;
 
       const float keyLeft = mKeyXPos.Get()[key];
-      const IRECT keyBounds{keyLeft, mRECT.T, keyLeft + blackKeyWidth, blackKeyBottom};
-      g.FillRect(blackOverlayColor, keyBounds, &mBlend);
+      const IRECT keyBounds{keyLeft, mRECT.T, keyLeft + geometry.blackKeyWidth, geometry.blackKeyBottom};
+      if(isSelected)
+        g.FillRect(selectedBlackFillColor, keyBounds, &mBlend);
+
+      if(isKeyNote)
+        DrawBlackKeyNoteDot(g, keyBounds, dotGeometry);
     }
   }
 
   bool mEditMode{false};
   int mSelectedMidiNote{-1};
   std::array<bool, 128> mHighlightedMidiNotes{};
-  IColor mUnselectedNonKeyNoteWhiteColor{0, 0, 0, 0};
-  IColor mUnselectedKeyNoteWhiteColor{90, 74, 128, 220};
-  IColor mSelectedNonKeyNoteWhiteColor{120, 255, 180, 70};
-  IColor mSelectedKeyNoteWhiteColor{150, 90, 195, 255};
-  IColor mUnselectedNonKeyNoteBlackColor{0, 0, 0, 0};
-  IColor mUnselectedKeyNoteBlackColor{145, 46, 105, 255};
-  IColor mSelectedNonKeyNoteBlackColor{155, 235, 182, 63};
-  IColor mSelectedKeyNoteBlackColor{185, 88, 160, 255};
+  IColor mKeyNoteDotColor{255, 32, 130, 245};
+  float mWhiteKeyDotRadiusFactor{0.4f};
 };
 } // namespace plugin_ui
