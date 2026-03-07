115784d7597e0de811e3ae15f386fc6afdc26b77
Ryan Wick <rrwick@gmail.com>
2026-03-07 12:20:08 +1100

    Add new set shape option

--- AdditiveWindSynth/src/ui/editor_tabs/level.h
+++ AdditiveWindSynth/src/ui/editor_tabs/level.h
@@ -6,6 +6,128 @@ namespace plugin_ui
 {
 namespace editor
 {
+inline double GetLevelPseudoLogRolloffIntensity(int oscillatorIndex)
+{
+  const double displayRampValue = 1.0 - (static_cast<double>(oscillatorIndex) / static_cast<double>(SimplePreset::kNumOscillators));
+  return transformations::NormalizedExp(displayRampValue, transformations::GetGlobalPseudoLogShapeValue());
+}
+
+inline bool IsPowerOfTwo(int value)
+{
+  return value > 0 && (value & (value - 1)) == 0;
+}
+
+template <std::size_t N>
+inline bool IsListedHarmonic(int harmonicIndex, const std::array<int, N>& harmonics)
+{
+  return std::binary_search(harmonics.begin(), harmonics.end(), harmonicIndex);
+}
+
+inline bool IsOctavesAndFifthsHarmonic(int harmonicIndex)
+{
+  static constexpr std::array<int, 13> kHarmonics{{1, 2, 3, 4, 6, 8, 12, 16, 24, 32, 48, 64, 96}};
+  return IsListedHarmonic(harmonicIndex, kHarmonics);
+}
+
+inline bool IsOctavesFifthsAndThirdsHarmonic(int harmonicIndex)
+{
+  static constexpr std::array<int, 18> kHarmonics{{1, 2, 3, 4, 5, 6, 8, 10, 12, 16, 20, 24, 32, 40, 48, 64, 80, 96}};
+  return IsListedHarmonic(harmonicIndex, kHarmonics);
+}
+
+inline bool ShouldApplyLevelShapeTopTaper(const char* shapeName)
+{
+  return std::strcmp(shapeName, "saw") == 0
+      || std::strcmp(shapeName, "square") == 0
+      || std::strcmp(shapeName, "triangle") == 0
+      || std::strcmp(shapeName, "flat") == 0;
+}
+
+inline bool TryGetLevelShapeIntensity(const char* shapeName, int oscillatorIndex, double& intensity)
+{
+  const double harmonicNumber = static_cast<double>(oscillatorIndex + 1);
+  const int harmonicIndex = oscillatorIndex + 1;
+
+  if(std::strcmp(shapeName, "sine") == 0)
+  {
+    intensity = (oscillatorIndex == 0) ? 1.0 : 0.0;
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "saw") == 0)
+  {
+    intensity = 1.0 / harmonicNumber;
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "square") == 0)
+  {
+    intensity = (oscillatorIndex % 2 == 0) ? (1.0 / harmonicNumber) : 0.0;
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "triangle") == 0)
+  {
+    intensity = (oscillatorIndex % 2 == 0) ? (1.0 / (harmonicNumber * harmonicNumber)) : 0.0;
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "flat") == 0)
+  {
+    intensity = 1.0;
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "rolloff") == 0)
+  {
+    intensity = GetLevelPseudoLogRolloffIntensity(oscillatorIndex);
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "rolloff odd") == 0)
+  {
+    intensity = (oscillatorIndex % 2 == 0) ? GetLevelPseudoLogRolloffIntensity(oscillatorIndex) : 0.0;
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "octaves") == 0)
+  {
+    intensity = IsPowerOfTwo(harmonicIndex) ? 1.0 : 0.0;
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "octaves+fifths") == 0)
+  {
+    intensity = IsOctavesAndFifthsHarmonic(harmonicIndex) ? 1.0 : 0.0;
+    return true;
+  }
+
+  if(std::strcmp(shapeName, "octaves+fifths+thirds") == 0)
+  {
+    intensity = IsOctavesFifthsAndThirdsHarmonic(harmonicIndex) ? 1.0 : 0.0;
+    return true;
+  }
+
+  return false;
+}
+
+inline bool ApplyLevelShape(SimplePreset& preset, const char* shapeName)
+{
+  for(int oscillatorIndex = 0; oscillatorIndex < SimplePreset::kNumOscillators; ++oscillatorIndex)
+  {
+    double intensity = 0.0;
+    if(!TryGetLevelShapeIntensity(shapeName, oscillatorIndex, intensity))
+      return false;
+
+    preset.SetOscillatorParameter(oscillatorIndex, OscillatorParameter::intensity, intensity);
+  }
+
+  if(ShouldApplyLevelShapeTopTaper(shapeName))
+    preset.ApplyIntensityTopTaper();
+
+  return preset.NormalizeIntensityWaveformRms();
+}
+
 inline void AppendLevelTabDescriptors(std::vector<OscillatorTabDescriptor>& descriptors)
 {
   descriptors.push_back({
@@ -155,33 +277,13 @@ inline void AttachLevelTabChildren(IVTabPage* page,
   });
 
   auto* setShapeControl = new ActionSelectionControl(
-    IRECT(), "choose shape", {"saw", "square", "triangle", "sine"}, styles.utilityDropdownText, styles.darkTab);
+    IRECT(), "choose shape", {"sine", "saw", "square", "triangle", "flat", "rolloff", "rolloff odd", "octaves", "octaves+fifths", "octaves+fifths+thirds"}, styles.utilityDropdownText, styles.darkTab);
   setShapeControl->SetOnSelection([context, sliderControl](const char* selectedText) {
     if(!selectedText)
       return;
 
     context->ApplyLevelActionToSelectedKeyNote(sliderControl, [selectedText](SimplePreset& preset) {
-      for(int oscillatorIndex = 0; oscillatorIndex < SimplePreset::kNumOscillators; ++oscillatorIndex)
-      {
-        const double harmonicNumber = static_cast<double>(oscillatorIndex + 1);
-        double intensity = 0.0;
-
-        if(std::strcmp(selectedText, "saw") == 0)
-          intensity = 1.0 / harmonicNumber;
-        else if(std::strcmp(selectedText, "square") == 0)
-          intensity = (oscillatorIndex % 2 == 0) ? (1.0 / harmonicNumber) : 0.0;
-        else if(std::strcmp(selectedText, "triangle") == 0)
-          intensity = (oscillatorIndex % 2 == 0) ? (1.0 / (harmonicNumber * harmonicNumber)) : 0.0;
-        else if(std::strcmp(selectedText, "sine") == 0)
-          intensity = (oscillatorIndex == 0) ? 1.0 : 0.0;
-        else
-          return false;
-
-        preset.SetOscillatorParameter(oscillatorIndex, OscillatorParameter::intensity, intensity);
-      }
-
-      preset.ApplyIntensityTopTaper();
-      return preset.NormalizeIntensityWaveformRms();
+      return ApplyLevelShape(preset, selectedText);
     });
   });
 
