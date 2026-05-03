4ff680d9ff940de271f0ff8dda7d31eed05413ee
Ryan Wick <rrwick@gmail.com>
2026-05-03 15:39:10 +1000

    Fix UIScene lifecycle for iOS

---
+++ Addivox/src/ios/SceneDelegate.m
@@ -0,0 +1,25 @@
+#import "SceneDelegate.h"
+
+@implementation SceneDelegate
+
+- (void)scene:(UIScene*)scene willConnectToSession:(UISceneSession*)session options:(UISceneConnectionOptions*)connectionOptions
+{
+}
+
+- (void)sceneDidBecomeActive:(UIScene*)scene
+{
+}
+
+- (void)sceneWillResignActive:(UIScene*)scene
+{
+}
+
+- (void)sceneWillEnterForeground:(UIScene*)scene
+{
+}
+
+- (void)sceneDidEnterBackground:(UIScene*)scene
+{
+}
+
+@end
